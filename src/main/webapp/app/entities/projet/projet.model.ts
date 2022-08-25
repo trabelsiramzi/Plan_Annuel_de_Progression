5d912c13b74e6f45133ba883d5cad6f7a3d8180e
TRABELSI Ramzi <ramzi.trabelsi@soprahr.com>
2022-08-25 02:45:12 +0100

    'emptyproject'

--- src/main/webapp/app/entities/projet/projet.model.ts
+++
@@ -1,9 +0,0 @@
-import { IAffectation } from 'app/entities/affectation/affectation.model';
-
-export interface IProjet {
-  id: number;
-  titreprojet?: string | null;
-  nomprojet?: Pick<IAffectation, 'id'> | null;
-}
-
-export type NewProjet = Omit<IProjet, 'id'> & { id: null };
--- src/main/webapp/app/entities/responsable/responsable.model.ts
+++
@@ -1,7 +0,0 @@
-export interface IResponsable {
-  id: number;
-  nommanager?: string | null;
-  nomteamlead?: string | null;
-}
-
-export type NewResponsable = Omit<IResponsable, 'id'> & { id: null };
